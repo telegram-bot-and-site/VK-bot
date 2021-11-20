704cd30bac1fbf8e428d8e84e5bb62a991f111e3
sannelo <alwexsyper@gmail.com>
2021-11-20 20:06:48 +0300

    releas

--- functions.py
+++ functions.py
@@ -1,18 +1,169 @@
 import redis
+import time
+import datetime
+import logging
+import json 
+
+from config import DB_HOST, DB_PASS, DB_PORT
 
 class BD:
-    def __init__(self, redis = None) -> None:
-        print(redis)
-        pass
+    def __init__(self) -> None:
+        try:
+            redis_cl = redis.Redis(host=DB_HOST,
+                                   port=DB_PORT,
+                                   db=0,
+                                   password=DB_PASS)
+            redis_cl.ping()
+        except:
+            print("FATAL ERROR REDIS CONNECTION")
+            logging.error("FATAL ERROR REDIS CONNECTION")
+            quit()
+
+        self.redis_cl = redis_cl
+
+        print("CONNECTION SUCCESSFUL")
+        logging.info("CONNECTION SUCCESSFUL")
+    
+    def get_payment(self) -> str:
+        return self.redis_cl.get("_PaymentInfo").decode('utf-8')
+    
+    def edit_payment(self, info: str):
+        self.redis_cl.set("_PaymentInfo", info)
+        return f"Инструкция обнавлена на:\n{info}"
+
+    def get_users(self):
+        tmp_str = ""
+
+        for id in self.redis_cl.keys():
+            if id == "_PaymentInfo": continue
+            try:
+                name = json.loads(self.redis_cl.get(id))["name"]
+                tmp_str += f"({id.decode('utf-8')}) {name}\n"
+            except:
+                continue
+        
+        return tmp_str
+
+    def get_user(self, id: str):
+        try:
+            id = int(id)
+        except:
+            return "Не верный id, короче ошибка :/"
+        
+        if self.redis_cl.exists(id) == 0:
+            return "Не верный id, короче ошибка :/"
+
+        data: dict = json.loads(self.redis_cl.get(id))
+
+        tmp_str = f"{id}\n"
+
+        for value in data.values():
+            tmp_str += f"{value}\n"
+
+        return tmp_str
+
+    def edit_user(self, info: str):
+        try:
+            values = str.split(info, "\n")
+            id = int(values[0])
+            tmp_jsonstr = self.redis_cl.get(id)
+        except:
+            return "Не верный id/формат строки, короче ошибка :/"
+
+        tmp_json: dict = json.loads(tmp_jsonstr)
+
+        # tmp_out = {}
+
+        for key, value in zip(tmp_json, values[1:]):
+            if value == '/ ': continue
+            tmp_json[key] = value
+            
+        self.redis_cl.set(id, json.dumps(tmp_json))
+
+        return f"Пользователь с айди ({id}) изменен!"
+    
+    def new_user(self, id: str):
+        try:
+            id = int(id)
+        except:
+            return "Не верный id, короче ошибка :/"
+        
+        tmp_user = {
+            "name": "none",
+            "age": 0,
+            "course": "none",
+            "lvl": "none",
+            "sex": "none", # М/Ж
+            "parent_name": "none",
+            "parent_numbet": "none",
+            "home_work": "none",
+
+            "lessons_have": 0,
+            "lesson": "none"
+        }
+
+        user = json.dumps(tmp_user)
+
+        self.redis_cl.set(id, user)
+
+        return f"Пользователь с айди ({id}) создан!"
     
     def get_lessons(self, id: int) -> str:
-        lessons = []
-        pass
+        data = self.redis_cl.get(id)
+
+        if data == None: return "ERROR :/"
+
+        info = json.loads(data)
+        
+        l_string = "   ✏ Расписание занятий\n"
+        l_number = 1
+
+        for lesson in info["lessons"]:
+
+            time = datetime.datetime.utcfromtimestamp(lesson["time"]).strftime("%d/%m/%Y-%H:%M")
+            home_work = "" if lesson["home_work"] == None else lesson["home_work"]
+            lesson_paid = "✅" if l_number <= info["lessons_have"] else "❌"
+
+            l_string += f"-> {lesson_paid} {time} - {home_work}\n"
+            l_number += 1
+        
+        l_string += f"Оплачено: {info['lessons_have']} занятия"
+        
+        # unixtime = time.mktime(lessons["time"]) datetime to UNIX time 
+        # dt = datetime.datetime.strptime("20/10/2020-15:00", "%d/%m/%Y-%H:%M") String to time :\
+        
+        return l_string
+
+    def get_lesson(self, id: int) -> str:
+        data = self.redis_cl.get(id)
+
+        if data == None: return "ERROR :/"
+
+        info = json.loads(data)
+
+        '''
+        Расписание занятий
+        -> Каждую пятницу в 17:00
+        -> Домашнее задание: бла-бла
+        -> Осталось уроков: 1
+        '''
+
+        return f'''О занятиях
+-> {info["lesson"]}
+-> Домашние задание: {info["home_work"]}
+-> Осталось занятий: {"Заморожено" if int(info["lessons_have"]) == -1 else (f"{info['lessons_have']} (Не забудьте оплатить)" if int(info["lessons_have"]) != 0  else info['lessons_have'])}'''
+#                   -> Комментарий: {info["comment"]}
 
     def get_profile(self, id: int) -> str:
-        info = "test_info"
-        return (f"-> Имя ученика: {info}" # Николай
-            f"\n-> Возраст: {info}" # 13
-            f"\n-> Курс: {info}" # Scratch/Construct/Godot
-            f"\n-> Уровень: {info}" # Начальный
-            f"\n-> Комментарии: {info}")
+        data = self.redis_cl.get(id)
+
+        if data == None: return "ERROR :/"
+
+        info = json.loads(data)
+        
+        return f'''Личный кабинет
+-> Имя ученика: {info['name']}
+-> Возраст: {info['age']}
+-> Курс: {info['course']}
+-> Уровень: {info['lvl']}'''
+              # -> Комментарии: {info['comment']}
--- main.py
+++ main.py
@@ -1,37 +1,134 @@
+from asyncio.windows_events import NULL
 import redis
+import logging
+import datetime
 
 from config import *
 from functions import BD
-from vkbottle.bot import Bot, Message
 
+from vkbottle.bot import Bot, Message
 from vkbottle import Keyboard, Text
 
 bot = Bot(token=TOKEN)
 
-users = [162099941, 320349065]
+date_str = datetime.datetime.now().strftime('%m-%d-%y_%H_%M')
+logger = logging.basicConfig(filename=f"logs\\{date_str}_log.log", level=logging.INFO)
 
-redis_cl = redis.Redis(host=DB_HOST, port=DB_PORT, db=0, password=DB_PASS)
+cash = {}
 
-print(redis_cl.dump("test.rdb"))
+# for admin in ADMINS:
+#     cash[admin] = ""
 
-bd = BD(redis_cl)
+bd = BD()
 
-keyboard = Keyboard(one_time=False).add(Text("-> 📌 Личный кабинет", {"profile": "local"})
-                            ).row().add(Text("-> ✏ Расписание занятий", {"lessons": "local"})
-                            ).row().add(Text("-> 💳 Оплатить", {"payment": "local"})
-                            ).row().add(Text("-> ❓ Задать вопрос", {"question": "local"})
-                            ).get_json()
+user_keyboard = Keyboard(one_time=False).add(Text("-> 📌 Личный кабинет", {"profile": "local"})
+                                 ).row().add(Text("-> ✏ О занятиях", {"lessons": "local"})
+                                 ).row().add(Text("-> 💳 Оплатить", {"payment": "local"})
+                                 ).row().add(Text("-> ❓ Задать вопрос", {"question": "local"})
+                                 ).get_json()
 
-@bot.on.message(payload={"profile": "local"})
+admin_keyboard = Keyboard(one_time=False).add(Text("-> Создать ученика", {"new_user": "local"})
+                                  ).row().add(Text("-> Изменить ученика", {"edit_user": "local"})
+                                  ).row().add(Text("-> Ученики/Users", {"get_users": "local"})
+                                  ).row().add(Text("-> Ученик/User", {"get_user": "local"})
+                                  ).row().add(Text("-> Информация о оплате", {"get_payment": "local"})
+                                  ).row().add(Text("-> Изменить информацию о оплате", {"edit_payment": "local"})
+                                  ).get_json()
+
+@bot.on.private_message(payload={"profile": "local"})
 async def profile(msg: Message):
-    await msg.answer(bd.get_profile(msg.from_id), keyboard=keyboard)
+    #print("Profile " + msg.from_id)
+    await msg.answer(bd.get_profile(msg.from_id), keyboard=user_keyboard)
 
-@bot.on.message(payload={"lessons": "local"})
+@bot.on.private_message(payload={"lessons": "local"})
 async def lessons(msg: Message):
-    await msg.answer(bd.get_lessons(msg.from_id), keyboard=keyboard)
+    #print("Lessons " + msg.from_id)
+    await msg.answer(bd.get_lesson(msg.from_id), keyboard=user_keyboard)
 
-@bot.on.message()
+
+@bot.on.private_message(payload={"payment": "local"})
+async def lessons(msg: Message):
+    await msg.answer(bd.get_payment(), keyboard=user_keyboard)
+
+
+@bot.on.private_message(payload={"question": "local"})
+async def lessons(msg: Message):
+    cash[msg.from_id] = "question"
+    await msg.answer("Напишите свой вопрос:", keyboard=user_keyboard)
+
+
+@bot.on.private_message(payload={"get_payment": "local"})
+async def lessons(msg: Message):
+    await msg.answer(bd.get_payment(), keyboard=admin_keyboard)
+
+
+@bot.on.private_message(payload={"edit_payment": "local"})
+async def lessons(msg: Message):
+    cash[msg.from_id] = "edit_payment"
+    await msg.answer("Введите новую информацию о полате:", keyboard=admin_keyboard)
+
+
+@bot.on.private_message(payload={"new_user": "local"})
+async def lessons(msg: Message):
+    cash[msg.from_id] = "new_user"
+    await msg.answer("Введите VKid нового пользователя:", keyboard=admin_keyboard)
+
+
+@bot.on.private_message(payload={"get_user": "local"})
+async def lessons(msg: Message):
+    cash[msg.from_id] = "get_user"
+    await msg.answer("Введите VKid пользователя:", keyboard=admin_keyboard)
+
+
+@bot.on.private_message(payload={"get_users": "local"})
+async def lessons(msg: Message):
+    await msg.answer(bd.get_users(), keyboard=admin_keyboard)
+
+
+@bot.on.private_message(payload={"edit_user": "local"})
+async def lessons(msg: Message):
+    cash[msg.from_id] = "edit_user"
+    await msg.answer("""Введите новую информацию формата:
+VKid ученика
+Имя ученика
+Возраст ученика
+Курс ученика
+Уровень обучения ученика
+Пол ученика
+Имя родителя
+Номер родителя
+Домашние задание""", keyboard=admin_keyboard)
+
+
+@bot.on.private_message()
 async def msg(msg: Message):
-    await msg.answer("Привет! 👋🏻\nЧто бы ты хотел узнать? 👀", keyboard=keyboard)
+    if msg.from_id in ADMINS:
+        if (msg.from_id not in cash) or cash[msg.from_id] == "":
+            await msg.answer("Пожалуйста, используйте меню", keyboard=admin_keyboard)
+        elif cash[msg.from_id] == "new_user":
+            cash[msg.from_id] = ""
+            await msg.answer(bd.new_user(msg.text), keyboard=admin_keyboard)
+            return
+        elif cash[msg.from_id] == "edit_user":
+            cash[msg.from_id] = ""
+            await msg.answer(bd.edit_user(msg.text), keyboard=admin_keyboard)
+            return
+        elif cash[msg.from_id] == "get_user":
+            cash[msg.from_id] = ""
+            await msg.answer(bd.get_user(msg.text), keyboard=admin_keyboard)
+            return
+        elif cash[msg.from_id] == "edit_payment":
+            cash[msg.from_id] = ""
+            await msg.answer(bd.edit_payment(msg.text), keyboard=admin_keyboard)
+            return
+    else:
+        if (msg.from_id not in cash) or cash[msg.from_id] == "":
+            await msg.answer("Пожалуйста, используйте меню", keyboard=user_keyboard)
+        elif cash[msg.from_id] == "question":
+            cash[msg.from_id] = ""
+            await msg.answer("Вопрос задан, ждите ответа от администратора!", keyboard=user_keyboard)
+            for id in ADMINS:
+                await bot.api.messages.send(id, 0, message=f"@id{msg.from_id} вопрос:\n{msg.text}")
+
 
 bot.run_forever()
